b9cad6ac36effdf0e3aae6a7527f1d30dccb11c8
renny-kunjumon <renny.kunjumon@rws.com>
2020-10-15 15:18:11 +0530

    2 files modified, 2 added, 1 deleted

---
+++ src/app/accordion/accordion-tab/accordion-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-tab',
+  templateUrl: './accordion-tab.component.html',
+  styleUrls: ['./accordion-tab.component.css']
+})
+export class AccordionTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { AccordionModule } from "primeng/accordion";
 import { AccordionComponent } from "./accordion/accordion.component";
+import { AccordionTabComponent } from "./accordion/accordion-tab/accordion-tab.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AccordionModule],
-  declarations: [AppComponent, AccordionComponent],
+  declarations: [AppComponent, AccordionComponent, AccordionTabComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
