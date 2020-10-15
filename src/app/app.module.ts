bb0ec669fb01395d3944795d4fbdb765d5ad5cbd
renny-kunjumon <renny.kunjumon@rws.com>
2020-10-15 15:10:22 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { AccordionModule } from "primeng/accordion";
+import { AccordionComponent } from "./accordion/accordion.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AccordionModule],
+  declarations: [AppComponent, AccordionComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
